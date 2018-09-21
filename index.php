6a59504dea2f51c261e39c5634e08096c51b5314
Snaddyvitch-Dispenser <28988626+Snaddyvitch-Dispenser@users.noreply.github.com>
2018-09-21 23:50:33 +0100

    Fix makepost to writepost file index

--- index.php
+++ index.php
@@ -5,4 +5,4 @@ $actual_link = isset($_SERVER['HTTPS']) ? "https" : "http";
 
 $actual_link .= "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-if (!isset($_SESSION['code'])) {require "notLoggedIn.php";} else {require "makePost.php";}; ?>
+if (!isset($_SESSION['code'])) {require "notLoggedIn.php";} else {require "writepost.php";}; ?>
