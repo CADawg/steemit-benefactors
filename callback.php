88be9ced3cc8b2a2e9a47668e1b3933cf649a441
Snaddyvitch-Dispenser <28988626+Snaddyvitch-Dispenser@users.noreply.github.com>
2018-09-21 23:42:27 +0100

    Fix Missing Function generateRandomString

--- callback.php
+++ callback.php
@@ -1,5 +1,15 @@
 <?php
 
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
 $lifetime = 604800;
 session_start();
 setcookie(session_name(), session_id(), time() + $lifetime, "/");
